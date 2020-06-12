543a2d164a95255c486989db4c921be5fa1f42ff
Ali Gamal Ali <hictors@gmail.com>
2020-06-12 04:39:25 +0200

    font in home fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,10 @@
   <div class="home">
     <div class="hero flex justify-center items-center mt-16 ">
       <div class="Description w-1/2 text-justify ml-10  ">
-        <h1 v-bold class="description__title text-center text-5xl mb-16 text ">
+        <h1 v-bold class="description__title text-center text-5xl mb-12 capitalize  ">
           welcome to the Game solution
         </h1>
-        <p class="description__paragraph text-lg text-justify font-mono">
+        <p class="description__paragraph text-lg text-justify font">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
           eaque quod nesciunt, adipisci et quidem expedita tempora ipsa
           doloribus perspiciatis porro nostrum voluptatem ea veniam, optio
